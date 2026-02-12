1f8a0a45222288b520eb738eef39a923d1a9abb9
Mohit Kumar <mk183018@gmail.com>
2026-02-12 13:40:14 +0530

    2nd commit

--- flightapp/views.py
+++ flightapp/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from flightapp.models import FlightDetails
 from flightapp.serializer import FlightDetailsSerializer
 from django.http import JsonResponse
+from rest_framework import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from flightapp.serializer import FlightDetailsSerializer
 
 # Create your views here.
+@csrf_exempt
 def FlightAPIViews(request):
     if request.method=='GET':
         flights=FlightDetails.objects.all()
         flight_data=FlightDetailsSerializer(flights,many=True)
         return JsonResponse(flight_data.data,safe=False)
     elif request.method=='POST':
-        pass
+        flights=JSONParser().parse(request)
+        flight_serializer=FlightDetailsSerializer(data=flights)
+        if flight_serializer.is_valid():
+            flight_serializer.save()
+            return JsonResponse('Record added Sucessfully',safe=False)
+
+
